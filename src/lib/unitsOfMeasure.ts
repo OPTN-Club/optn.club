420e8295d46577050b45a7618a1cd9accf0d67f1
Aaron Throckmorton <aaron.throckmorton@stgconsulting.com>
2022-02-11 14:19:41 -0700

    Change from contextual unit order to fixed order

--- src/lib/unitsOfMeasure.ts
+++ src/lib/unitsOfMeasure.ts
@@ -26,32 +26,27 @@ export function orderUnitValues<T extends UnitValues<string>, U extends UnitOfMe
 
 export function formatPressure(pressure: string | number, unit: PressureUnit, precision = 1, includeUnit = false) {
   const values = convertPressureFrom(pressure, unit);
-  const formatted: UnitValues<string> = {
-    [PressureUnit.bar]: formatFloat(values.bar, precision, ' bar', includeUnit),
-    [PressureUnit.psi]: formatFloat(values.psi, precision, ' psi', includeUnit),
-  };
-  return orderUnitValues(formatted, unit);
+  return [
+    formatFloat(values.bar, 2, ' bar', includeUnit),
+    formatFloat(values.psi, precision, ' psi', includeUnit),
+  ];
 }
 
 export function formatForce(force: string | number, unit: ForceUnit, precision = 1, includeUnit = false) {
   const values = convertForceFrom(force, unit);
-  const formatted = {
-    newtons: formatFloat(values.newtons, precision, ' n/mm', includeUnit),
-    kgf: formatFloat(values.kgf, precision, ' kgf/mm', includeUnit),
-    lbs: formatFloat(values.lbs, precision, ' lbs/in', includeUnit),
-  };
-
-  return orderUnitValues(formatted, unit);
+  return [
+    formatFloat(values.kgf, precision, ' kgf/mm', includeUnit),
+    formatFloat(values.lbs, precision, ' lbs/in', includeUnit),
+    formatFloat(values.newtons, precision, ' n/mm', includeUnit),
+  ];
 }
 
 export function formatLength(length: string | number, unit: LengthUnit, precision = 1, includeUnit = false) {
   const values = convertLengthFrom(length, unit);
-  const formatted = {
-    cm: formatFloat(values.cm, precision, ' cm', includeUnit),
-    in: formatFloat(values.in, precision, ' in', includeUnit),
-  };
-
-  return orderUnitValues(formatted, unit);
+  return [
+    formatFloat(values.cm, precision, ' cm', includeUnit),
+    formatFloat(values.in, precision, ' in', includeUnit),
+  ];
 }
 
 export function isPressureUnit(unit: UnitOfMeasure): unit is PressureUnit {
@@ -86,15 +81,21 @@ export function formatUnit(value: string | number, unit: UnitOfMeasure, precisio
   throw new Error(`Invalid Unit of Measure: ${unit}`);
 }
 
+const unitHeaders = {
+  force: ['kgf/mm', 'lbs/in', 'n/mm'],
+  pressure: ['bar', 'psi'],
+  length: ['cm', 'in'],
+};
+
 export function formatUnitHeaders(unit: UnitOfMeasure) {
   if (isPressureUnit(unit)) {
-    return orderUnitValues(pressureUnitHeaders, unit);
+    return unitHeaders.pressure;
   }
   if (isForceUnit(unit)) {
-    return orderUnitValues(forceUnitHeaders, unit);
+    return unitHeaders.force;
   }
   if (isLengthUnit(unit)) {
-    return orderUnitValues(lengthUnitHeaders, unit);
+    return unitHeaders.length;
   }
   throw new Error(`Invalid Unit of Measure: ${unit}`);
 }
