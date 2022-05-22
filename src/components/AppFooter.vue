5a32cd6de6f6c237ce2317c2a64f142045730b7f
Aaron Throckmorton <aaron.throckmorton@stgconsulting.com>
2022-05-22 10:58:03 -0600

    Add nav bar and footer text back in

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="text-center text-sm">
     Created by Root, SharpSeeEr, and Doubiez.
-    <!-- Tune Calculator based off of
-    <a href="https://www.reddit.com/user/diquee/" target="_blank">u/diquee's</a> tune calculator spreadsheet -->
+    Tune Calculator based off of
+    <a href="https://www.reddit.com/user/diquee/" target="_blank">u/diquee's</a> tune calculator spreadsheet
   </footer>
 </template>
