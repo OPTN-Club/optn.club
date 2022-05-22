4248489420dc73d0b99abaebed4a5fbe417b16eb
Aaron Throckmorton <aaron.throckmorton@stgconsulting.com>
2022-05-22 11:50:29 -0600

    Fix server.js to serve files when ForzaOpenTuneFormatter is in path

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const path = require('path');
 
 const app = express();
 
+const staticMiddleware = express.static(path.join(__dirname, '/docs'));
+const spaMiddleware = (req, res) => {
+  res.sendFile(path.join(__dirname, '/docs/index.html'));
+}
+
 app.set('x-powered-by', false);
 app.use(compression());
-app.use('/', express.static(path.join(__dirname, '/docs')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/docs/index.html'))
-);
+app.use('/', staticMiddleware);
+app.use('/ForzaOpenTuneFormatter', staticMiddleware);
+app.use(spaMiddleware);
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/docs/index.html'))
+// );
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.info(`Express server listening on port ${port}.`));
