efc8b14c3b0c1a8bda6d7a3fca019962070c23c8
Aaron Throckmorton <aaron.throckmorton@stgconsulting.com>
2022-07-06 11:46:38 -0600

    Update vite config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,11 @@
-import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import TuneFormatter from '../components/formatter/TuneFormatter.vue';
 import MakesAndModels from '../components/MakesAndModels.vue';
 import TuningChart from '../components/chart/TuningChart.vue';
 import TuneCalculator from '../components/tune-calculator/TuneCalculator.vue';
 
-const history = import.meta.env.NODE_ENV === 'github'
-  ? createWebHashHistory(import.meta.env.BASE_URL)
-  : createWebHistory(import.meta.env.BASE_URL);
-
 const router = createRouter({
-  history,
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       name: 'formatter',
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import vue from '@vitejs/plugin-vue'
 import { build, defineConfig } from 'vite'
 
+console.log(process.env.NODE_ENV);
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3333,
   },
-  base: process.env.NODE_ENV === 'github' ? '/optn.club/' : '/',
+  base: '/', // process.env.NODE_ENV === 'github' ? '/optn.club/' : '/',
   build: {
     outDir: 'docs',
   },
