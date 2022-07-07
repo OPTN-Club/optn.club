f59245ba9dfedda77e97ac792e5ae1412d342fd8
Julian Osborne <subaruforge@gmail.com>
2022-07-07 14:18:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,12 +13,16 @@ app.set('x-powered-by', false);
 app.use(compression());
 app.use('/', staticMiddleware);
 app.use('/optn.club', staticMiddleware);
+
+// Telemetry API test, returns documentation as [[name(str), byte length(int), data type(str), description(str), optional calculations(str) OR null]]
+app.get('/api/v1/telemetry', (req,res) =>
+  res.sendFile(path.join(__dirname, '/telemetry_docs.json'))
+);
+
 app.use(spaMiddleware);
 // app.get('*', (req, res) =>
 //   res.sendFile(path.join(__dirname, '/docs/index.html'))
 // );
-app.get('/api/v1/telemetry', (req,res) =>
-  res.sendFile(path.join(__dirname, '/telemetry_docs.json'))
-);
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.info(`Express server listening on port ${port}.`));
