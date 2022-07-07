f19456cec1f56065230e59fcfc1299b79f100a3a
Julian Osborne <subaruforge@gmail.com>
2022-07-07 14:01:18 -0400

    Add telemetry API documentation json

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(spaMiddleware);
 // app.get('*', (req, res) =>
 //   res.sendFile(path.join(__dirname, '/docs/index.html'))
 // );
-
+app.get('/api/v1/telemetry', (req,res) =>
+  res.sendFile(path.join(__dirname, '/telemetry_docs.json'))
+);
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.info(`Express server listening on port ${port}.`));
