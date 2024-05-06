fab272aa29767d23f7b4fa23a608e5794168c64b
turbodoubiez <romberg118+github_alt@gmail.com>
2024-05-06 10:21:28 +0200

    fix(*): Move URLs to single file, fix Discord Invite

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,5 @@
 <template>
   <footer class="text-center text-sm">
     Created by [Root, SharpSeeEr, Doubiez]
-    <!-- Tune Calculator based off of
-    <a href="https://www.reddit.com/user/diquee/" target="_blank">u/diquee's</a> tune calculator spreadsheet -->
   </footer>
 </template>
