480339a2ba0bd7ba6191f953c2ba46a8275b1c0a
turbodoubiez <romberg118+github_alt@gmail.com>
2025-02-10 13:42:15 +0100

    chore: update eslint ruleset

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
   ],
 
   rules: {
+    'import/no-extraneous-dependencies': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
@@ -86,7 +87,7 @@ module.exports = {
     }],
     'import-newlines/enforce': [
       'error',
-      { items: 2 },
+      { items: 5 },
     ],
     'import/extensions': [
       'error',
